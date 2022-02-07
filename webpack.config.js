2c645afe2dd9edb9a88cc5a858333419543d7aa1
pdh <pdh@infoguide.co.kr>
2022-02-07 19:29:19 +0900

    run build html loader

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
                 use: ['file-loader?name=[name].[ext]'],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
             }
         ],
     },
